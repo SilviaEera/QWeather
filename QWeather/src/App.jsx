4f84b25da61c4513694dd4ce9f871be02ac294a3
Fei <78798119+SilviaEera@users.noreply.github.com>
2024-10-12 16:29:09 +0600

    added default weather

--- QWeather/src/App.jsx
+++ QWeather/src/App.jsx
@@ -1,8 +1,10 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faTemperatureFull } from "@fortawesome/free-solid-svg-icons";
 import "./index.css";
+
 const api_key = "9cce656fbdcd17f5f30dc28c379d780f";
+
 function App() {
   const inputRef = useRef(null);
   const [apiData, setApiData] = useState(null);
@@ -39,52 +41,57 @@ function App() {
     },
   ];
 
-  const fetchWeather = async () => {
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&appid=${api_key}`;
-
-    // console.log(inputRef.current.value);
-
-    fetch(URL)
-      .then((res) => res.json())
-      .then((data) => {
-        setShowWeather(
-          weathers.filter((weather) => weather.type === data.weather[0].main)
-        );
-        setApiData(data);
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const fetchWeather = async (city) => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    try {
+      const res = await fetch(URL);
+      const data = await res.json();
+      setShowWeather(
+        weathers.filter((weather) => weather.type === data.weather[0].main)
+      );
+      setApiData(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   };
-  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetchWeather("Kyoto");
+  }, []);
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      fetchWeather(inputRef.current.value);
+    }
+  };
 
   return (
     <>
-      <div className="bg-gray-800 grid h-screen  place-items-center">
+      <div className="bg-gray-800 grid h-screen place-items-center">
         <div className="bg-white w-72 rounded-lg p-4">
           <div className="flex items-center justify-between">
             <input
               type="text"
               ref={inputRef}
-              id=""
+              onKeyPress={handleKeyPress}
               placeholder="Enter City Name"
               className="border-b border-gray-200 focus:outline-none text-xl p-2 w-32 flex-1"
             />
-            <button onClick={fetchWeather}>
+            <button onClick={() => fetchWeather(inputRef.current.value)}>
               <FontAwesomeIcon
                 icon={faSun}
-                className="text-xl text-gray-400  hover:text-gray-600"
+                className="text-xl text-gray-400 hover:text-gray-600"
               />
             </button>
           </div>
+
           {/* icon start */}
           <div>
             {showWeather && (
               <div className="text-center flex flex-col gap-6 mt-10">
                 {apiData && (
                   <p className="cityName text-center font-medium text-xl text-gray-400">
-                    {" "}
                     {apiData.name}, {apiData.sys.country}
                   </p>
                 )}
@@ -109,13 +116,8 @@ function App() {
                       &#176;F
                     </h2>
                   )}
-
-                  {/* <p className="font-thin">{apiData.weather[0].description}</p> */}
                 </div>
-                {/* <p className="font-bold text-xl text-gray-500">
-                  {" "}
-                  {apiData.weather[0].description}
-                </p> */}
+
                 <p className="font-bold text-xl text-gray-400">
                   Wind Speed: {(apiData.wind.speed * 3.6).toFixed(2)} km/h
                 </p>
